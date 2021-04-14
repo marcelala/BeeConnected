d02a040916036a51b91b79aeb226f8c0247d3717
Nicholas Hartman <hartn001@gmail.com>
2021-04-14 22:29:36 +0200

    Added controller needed to comment out so can link with Post

--- src/main/java/se/kth/sda/skeleton/comments/Comment.java
+++ src/main/java/se/kth/sda/skeleton/comments/Comment.java
@@ -1,8 +1,60 @@
 package se.kth.sda.skeleton.comments;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import se.kth.sda.skeleton.posts.Post;
 
 @Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // @JoinColumn(nullable = false)
+    private String body;
+
+    // @ManyToOne
+    // @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
+    // property = "id")
+    // @JsonIdentityReference(alwaysAsId = true)
+    // @JoinColumn(nullable = false)
+    // @NotNull
+    // private Post post;
+
+    public Comment() {
+    }
+
+    public Comment(String body) {
+
+        this.body = body;
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 
 }
