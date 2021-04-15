ccda3f6b38af585f9ea8b2abe2613a7633dd4a49
Nicholas Hartman <hartn001@gmail.com>
2021-04-15 15:11:32 +0200

    Created Post, Comment and User Associations

--- src/main/java/se/kth/sda/skeleton/comments/Comment.java
+++ src/main/java/se/kth/sda/skeleton/comments/Comment.java
@@ -4,6 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import se.kth.sda.skeleton.posts.Post;
+import se.kth.sda.skeleton.user.User;
 
 @Entity
 public class Comment {
@@ -13,6 +23,20 @@ public class Comment {
     private Long id;
 
     private String body;
+
+    @ManyToOne
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JoinColumn(nullable = false)
+    @NotNull
+    private Post commentOwner;
+
+    @ManyToOne
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "email")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JoinColumn(nullable = false)
+    @NotNull
+    private User userCommentOwner;
 
     public Comment() {
     }
@@ -37,6 +61,22 @@ public class Comment {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public Post getCommentOwner() {
+        return this.commentOwner;
+    }
+
+    public void setCommentOwner(Post commentOwner) {
+        this.commentOwner = commentOwner;
+    }
+
+    public User getUserCommentOwner() {
+        return this.userCommentOwner;
+    }
+
+    public void setUserCommentOwner(User userCommentOwner) {
+        this.userCommentOwner = userCommentOwner;
     }
 
 }
--- src/main/java/se/kth/sda/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda/skeleton/posts/Post.java
@@ -1,9 +1,22 @@
 package se.kth.sda.skeleton.posts;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import se.kth.sda.skeleton.comments.Comment;
+import se.kth.sda.skeleton.user.User;
 
 // @TODO add Hibernate annotations
 @Entity
@@ -13,8 +26,17 @@ public class Post {
     private Long id;
 
     private String body;
-
     private String title;
+
+    @ManyToOne
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "email")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JoinColumn(nullable = false)
+    @NotNull
+    private User postOwner;
+
+    @OneToMany(mappedBy = "commentOwner")
+    private List<Comment> comments;
 
     public Post() {
     }
@@ -45,6 +67,22 @@ public class Post {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public User getPostOwner() {
+        return this.postOwner;
+    }
+
+    public void setPostOwner(User postOwner) {
+        this.postOwner = postOwner;
+    }
+
+    public List<Comment> getComments() {
+        return this.comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 
 }
