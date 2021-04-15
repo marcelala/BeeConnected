c16e93e13cb96d69212702f7331d963f153fdc75
Nicholas Hartman <hartn001@gmail.com>
2021-04-15 10:23:19 +0200

    Fixed Entity on class and added Id

--- src/main/java/se/kth/sda/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda/skeleton/posts/Post.java
@@ -1,7 +1,15 @@
 package se.kth.sda.skeleton.posts;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 // @TODO add Hibernate annotations
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String body;
--- src/main/java/se/kth/sda/skeleton/posts/PostController.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostController.java
@@ -5,7 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.kth.sda.skeleton.ResourceNotFoundException;
+import se.kth.sda.skeleton.user.User;
+import se.kth.sda.skeleton.user.UserRepository;
+import se.kth.sda.skeleton.user.UserService;
 
+import java.security.Principal;
 import java.util.List;
 
 /*
@@ -18,39 +22,54 @@ public class PostController {
 
     PostRepository postRepository;
     PostService postService;
+    UserService userService;
 
     @Autowired
-    public PostController(PostRepository postRepository, PostService postService) {
+    public PostController(PostRepository postRepository, PostService postService, UserService userService) {
         this.postRepository = postRepository;
         this.postService = postService;
+        this.userService = userService;
     }
 
-    @GetMapping //should return all posts.
-    public List<Post> listAllPosts(){
+    @GetMapping // should return all posts.
+    public List<Post> listAllPosts() {
         List<Post> posts = postRepository.findAll();
         return posts;
     }
 
-    @GetMapping("/{id}") //should return a specific post based on the provided id.
-    public ResponseEntity<Post> getArticle(@PathVariable Long id){
+    @GetMapping("/{id}") // should return a specific post based on the provided id.
+    public ResponseEntity<Post> getArticle(@PathVariable Long id) {
         Post post = postRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
         return ResponseEntity.ok(post);
     }
 
-    @PostMapping //should create a new post
-    public ResponseEntity<Post> createPost(@RequestBody Post post){
+    @PostMapping // should create a new post
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
         postRepository.save(post);
         return ResponseEntity.status(HttpStatus.CREATED).body(post);
     }
 
-    @PutMapping("/{id}") //should update the post based on the provided id
-    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post updatedPost){
+    // @PostMapping // should create a new post
+    // public ResponseEntity<Post> createPost(@RequestBody Post post, Principal
+    // principal) {
+    // String userName = principal.getName();
+    // User user = userService.findUserByEmail(userName);
+    // System.out.println(user.getName());
+    // postRepository.save(post);
+    // return ResponseEntity.status(HttpStatus.CREATED).body(post);
+    // }
+
+    @PutMapping("/{id}") // should update the post based on the provided id
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post updatedPost) {
         Post post = postService.updatePost(id, updatedPost);
         return ResponseEntity.ok(post);
     }
 
-    @DeleteMapping("/{id}") //should delete the post based on the provided id
-    public ResponseEntity<Post> deleteArticle(@PathVariable Long id){
+    @DeleteMapping("/{id}") // should delete the post based on the provided id
+    public ResponseEntity<Post> deleteArticle(@PathVariable Long id, Principal principal) {
+        String userName = principal.getName();
+        User user = userService.findUserByEmail(userName);
+        System.out.println(user.getName());
         Post post = postRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
         postRepository.delete(post);
         return ResponseEntity.ok(post);
