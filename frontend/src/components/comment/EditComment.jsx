1a1f18154c079853f88a86c229a6092ec5dca6a7
Nicholas Hartman <hartn001@gmail.com>
2021-04-19 00:40:15 +0200

    Changed edit to update

--- frontend/src/components/comment/EditComment.jsx
+++ frontend/src/components/comment/EditComment.jsx
@@ -24,7 +24,7 @@ export default function EditComment({ onSubmit, comment }) {
       </div>
       <div className="form-group">
         <button className="btn post comment" onClick={handleSubmit}>
-          Edit
+          Update
         </button>
       </div>
     </form>
