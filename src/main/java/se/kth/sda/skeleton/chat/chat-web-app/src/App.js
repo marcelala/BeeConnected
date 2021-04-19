59a375131edce27b4d8deaffbab3266470addd95
YADISH BUKHARI <yadish@live.com>
2021-04-19 13:10:12 +0200

    App.js file edited

--- src/main/java/se/kth/sda/skeleton/chat/chat-web-app/src/App.js
+++ src/main/java/se/kth/sda/skeleton/chat/chat-web-app/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Chat from './features/Chat/Chat';
+import Login from './features/Login/Login';
+import Paper from '@material-ui/core/Paper';
+import {makeStyles} from '@material-ui/core/styles';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  content: {
+    width: '100%',
+    maxWidth: '750px',
+    margin: '1rem'
+  }
+}));
 
 function App() {
+
+  const classes = useStyles();
+  const [userName, setUserName] = useState('');
+
+  const connect = (userName) => {
+    setUserName(userName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Paper elevation={3} className={classes.content}>
+        {!userName && <Login onConnect={connect}/>}
+        {userName && <Chat userName={userName}/>}
+      </Paper>
     </div>
   );
 }
